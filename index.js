c07b853d890d5d82e430504c0d8fe6c97277a898
pixelcarnival <112376989+pixelcarnival@users.noreply.github.com>
2022-11-26 13:01:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+import './index.css'
+import checkCookies from './src/app'
+
+new checkCookies('.cookie-consent') // передача в спараметр конструктора нашей дива с информацией по классу.
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const COOKIE_CONSENT_KEY = 'cookie-consent-allowed'
+console.log(COOKIE_CONSENT_KEY)
